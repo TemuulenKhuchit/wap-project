1927c648a55f5f4eb4a536a7c436c8f3509480f0
Temuulen <temuulenkhuchit01@gmail.com>
2024-10-25 11:24:58 -0500

    Fixed getPopularWords

--- server/models/dictionaryModel.js
+++ server/models/dictionaryModel.js
@@ -1,15 +1,22 @@
 import DB from "./englishdictionary.json" assert { type: "json" };
 
-let popularWords = [];
+let popularWords = new Map();
 
 export const getDefinitions = (searchWord) => {
   return DB.entries.filter((row) => row.word && row.word.toLowerCase() === searchWord.toLowerCase());
 };
 
 export const addPopularWords = (searchWord) => {
-  popularWords.push(searchWord);
+  if (popularWords.has(searchWord)) {
+    popularWords.set(searchWord, popularTerms.get(searchWord) + 1);
+  } else {
+    popularWords.set(searchWord, 1);
+  }
 };
 
 export const getPopularWords = () => {
-  return [...new Set(popularWords)].slice(0, 10);
+  return Array.from(popularWords.entries())
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 10)
+    .map(([word]) => word);
 };
